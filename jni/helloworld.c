7d0f2752fe536f31fa22201d7368f546db209edb
James <jagleeso@gmail.com>
2013-10-27 13:15:07 -0400

    Add a script for sampling the time it takes for the GPU to timeout (as indicated by the phone crashing) on an OpenCL program that executes an infinite loop.
    Test various example programs containing infinite loops to see if they
    cause a crash:
    - for (; ;) {}
    - flip output[0] between '1' and '2', and alternate between reading
      input[0] and input[1]
    - flip entire output buffer between '1' and '2', and read the entire
      input array

--- jni/helloworld.c
+++ jni/helloworld.c
@@ -26,12 +26,18 @@ Copyright ©2012 Advanced Micro Devices, Inc. All rights reserved.
 
 #define MAX_CHARS "1000"
 
-#define ARRAY_SIZE (1024)
+/* #define ARRAY_SIZE (1024) */
+
 /* A sufficiently large ARRAY_SIZE will cause the device to crash and reboot (it will get past 
  * allocating and writig the input and allocating output memory, but crashes is mid-kernel 
  * execution).
+ *
+ * However, this no longer crashes when OpenGL shared libraries are not present, indicating there is some 
+ * background OpenGL related GPU activity going on.
  */
 // #define ARRAY_SIZE (5000 * 1024)
+
+#define ARRAY_SIZE (7500 * 1024)
 
 #define  KERNEL_SRC "__kernel void helloworld(__global char* in, __global char* out) {"\
 "	int num = get_global_id(0);"\
@@ -49,6 +55,8 @@ Copyright ©2012 Advanced Micro Devices, Inc. All rights reserved.
 "	for (i = 0; i < elems; i++) {"\
 "	    out[i] = in[i] + 1;"\
 "	}"\
+"   for (; ; ) {"\
+"   }"\
 "}"
 
 /* NOTE:
@@ -57,6 +65,43 @@ Copyright ©2012 Advanced Micro Devices, Inc. All rights reserved.
 "   for (; ; ) {"\
 "   }"\
  */ 
+
+
+/* NOTE:
+ * Maybe the driver is checking for memory accesses to detect timeouts.  Try writing the output buffer 
+ * repeatedly, or accessing the input buffer repeatedly; that is:
+ * out[0] = '0'
+ * out[0] = '1'
+ * out[0] = '0'
+ * ...
+ * READ in[0]
+ * READ in[1]
+ * READ in[0]
+ * ...
+ *
+"	int j = 0;"\
+"	char read_char;"\
+"   for (; ; ) {"\
+"	    j += 1;"\
+"	    j = j % 2;"\
+"	    out[0] = (char) j;"\
+"	    read_char = in[j];"\
+"   }"\
+ * STILL FAILS
+ */ 
+
+/* Perhaps this example is too simple (e.g. if it checks buffer read/write addresses, maybe we 
+ * happen to fall on the same address)... lets loop over the whole output array repeatedly and flip 
+ * between '1' and '2', and read the input array at each position:
+"	char read_char;"\
+"	for (i = 0; ; i = (i + 1) % elems) {"\
+"	    int out_int = out[i];"\
+"	    out_int = '1' + (( (out_int - '1') + 1 ) % 2);"\
+"	    out[i] = (char) out_int;"\
+"	    read_char = in[i];"\
+"	}"\
+ * STILL FAILS
+ */
 
 #define CHECK_STATUS(status, msg) \
 if (status != CL_SUCCESS) { \
@@ -187,6 +232,7 @@ int main(int argc, char* argv[])
     input[strlength - 1] = '\0';
 	printf("input string: %." MAX_CHARS "s\n",input);
 	char *output = (char*) malloc(strlength + 1);
+	printf("Reading an array input[%i] (%fK) of '1's ...\n", ARRAY_SIZE, (double)ARRAY_SIZE / 1024.0);
 
 	IAH();
     cl_event cpu_to_gpu_event;
@@ -261,6 +307,7 @@ int main(int argc, char* argv[])
      */
 	IAH();
     cl_event gpu_to_cpu_event;
+	printf("Reading an array output[%i] (%fK) of '2's ...\n", ARRAY_SIZE, (double)ARRAY_SIZE / 1024.0);
 	status = clEnqueueReadBuffer(commandQueue, outputBuffer, CL_TRUE, 0, strlength * sizeof(char), output, 0, NULL, &gpu_to_cpu_event);
     CHECK_STATUS(status, "clEnqueueReadBuffer");
     cl_ulong gpu_to_cpu_event_start, gpu_to_cpu_event_end, gpu_to_cpu_event_duration;
---
+++ sample_gpu_timeout.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Sample the time it takes for the GPU to timeout (as indicated by the phone crashing) on an OpenCL 
+# program that executes an infinite loop.
+
+SAMPLES=25
+# seconds to wait for device to reboot after crashing
+# SLEEP_UNTIL_REBOOT=5
+
+cd "$(dirname "$0")"
+# set -x
+set -e
+opencl=helloworld.cl
+executable=helloworld
+ndk-build
+
+copy_program_over() {
+    adb push libs/armeabi-v7a/$executable /data/local/tmp
+    adb push jni/$opencl /data/local/tmp
+}
+
+wait_for_device_reboot() {
+    while true; do
+        # poll until the device is available
+        adb devices | \
+            grep -v "List of devices attached" | \
+            grep "device" --quiet && \
+            break
+        sleep 1
+    done
+}
+
+seq 1 $SAMPLES | while read i; do 
+    copy_program_over
+    # http://stackoverflow.com/questions/2408981/how-cant-i-redirect-the-output-of-time-command
+    { time adb shell ./data/local/tmp/$executable; } 2>&1
+    echo
+    wait_for_device_reboot
+done
